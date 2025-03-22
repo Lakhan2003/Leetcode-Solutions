57331db467ffe68222673f028b3db8c4e406f7fc
RamLakhan Dhangar <138513313+Lakhan2003@users.noreply.github.com>
2025-03-22 10:05:26 +0530

    Time: 0 ms (100%), Space: 41.5 MB (50.53%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if (s.length() != goal.length())
-            return false;
+        if (s.length() != goal.length()) {
+            return false; // Ensure we return false if lengths don't match
+        }
 
         String combined = s + s;
 
-        if (combined.contains(goal))
+        // If goal is found in the combined string, return true
+        if (combined.contains(goal)) {
             return true;
-        else
-            return false;
+        }
 
+        // If goal is not found, return false
+        return false;
     }
 }
