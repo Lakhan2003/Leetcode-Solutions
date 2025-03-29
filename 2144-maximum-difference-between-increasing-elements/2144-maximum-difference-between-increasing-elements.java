b10293ca57e39bbe834121104330ee05ce215da9
RamLakhan Dhangar <138513313+Lakhan2003@users.noreply.github.com>
2025-03-29 20:17:33 +0530

    Time: 5 ms (33.07%), Space: 42.5 MB (13.8%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
@@ -1,28 +1,29 @@
-// class Solution {
-//     public int maximumDifference(int[] nums) {
-//         int max=-1;
-//         for(int i=0;i<nums.length;i++){
-//             for(int j=i+1;j<nums.length;j++){
-//                 if(nums[j]-nums[i]>max){
-//                     max=nums[j]-nums[i];
-//                 }
-//             }
-//         }
-//         return max;
-//     }
-// }
 class Solution {
     public int maximumDifference(int[] nums) {
-        int n = nums.length;
-        int max = -1;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[j] > nums[i]) {
-                    int diff = nums[j] - nums[i];
-                    max = Math.max(max, diff);
+        int max=-1;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]>nums[i]){
+                    int diff=nums[j]-nums[i];
+                    max=Math.max(max,diff);
                 }
             }
         }
         return max;
     }
 }
+// class Solution {
+//     public int maximumDifference(int[] nums) {
+//         int n = nums.length;
+//         int max = -1;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (nums[j] > nums[i]) {
+//                     int diff = nums[j] - nums[i];
+//                     max = Math.max(max, diff);
+//                 }
+//             }
+//         }
+//         return max;
+//     }
+// }
