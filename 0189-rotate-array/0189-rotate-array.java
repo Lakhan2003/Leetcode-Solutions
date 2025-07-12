57f0f6fd3dc02629111d54a110632d3f26168b43
RamLakhan Dhangar <138513313+Lakhan2003@users.noreply.github.com>
2025-07-12 10:38:21 +0530

    Time: 0 ms (100%), Space: 57.6 MB (22.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,29 +1,17 @@
 class Solution {
-    public void rotate(int[] nums, int d) {
+    public void rotate(int[] nums, int k) {
         int n = nums.length;
-
-        // // If the array is empty or has only one element, no need to rotate
-        // if (n <= 1) return;
-
-        // If d is greater than n, reduce it to the effective rotations needed
-        d = d % n;
-
-        // Step 1: Reverse the entire array
+        k = k % n;
         reverse(nums, 0, n - 1);
-
-        // Step 2: Reverse the first d elements
-        reverse(nums, 0, d - 1);
-
-        // Step 3: Reverse the remaining elements
-        reverse(nums, d, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
     }
 
-    // Helper function to reverse the array from index 'start' to 'end'
-    private void reverse(int[] arr, int start, int end) {
+    public void reverse(int[] nums, int start, int end) {
         while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
             start++;
             end--;
         }
